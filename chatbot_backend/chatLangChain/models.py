8e17b364325c794027a0b2285b3bae16473fc2ef
Amen Munir <amen_dev17@mexemai.com>
2025-06-13 12:53:57 +0500

    updated file structure

--- chatbot_backend/chatLangChain/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Question(models.Model):
-    prompt = models.CharField(max_length=256)
-    response = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.prompt
--- chatbot_backend/chatLangChain/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from .views import chat_with_prompt_template
-
-urlpatterns = [
-    path('chatai/', chat_with_prompt_template, name='chat-api'),
-]
